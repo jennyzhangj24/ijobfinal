7df164bdc4098e4ec0f4606d5166594fee290ed0
Jenny Zhang <jenny.zhang.la@gmail.com>
2014-12-06 16:15:46 -0500

    register commit

--- register.php
+++ register.php
@@ -9,12 +9,19 @@
 <body>
 	<section id="form">
 		<form name="form" method="post" action="register_finish.php">
-		<li>username:<input type="text" name="id" /> </li>
-		<li>password:<input type="password" name="pw" /> </li>
-		<li>password again:<input type="password" name="pw2" /></li>
-		<li>telephone:<input type="text" name="telephone" /></li>
-		<li>address:<input type="text" name="address" /></li>
-		<li>others:<textarea name="other" cols="45" rows="5"></textarea></li>
+		<li>Username:<input type="text" name="id" /> </li>
+		<li>Password:<input type="password" name="pw" /> </li>
+		<li>Password again:<input type="password" name="pw2" /></li>
+		<li>Gender:
+			<select name="Gender">
+				<option value="Male">Male</option>
+				<option value="Female">Female</option>
+			</select>
+		</li>
+		<li>Telephone:<input type="text" name="telephone" /></li>
+		<li>Address:<input type="text" name="address" /></li>
+		<li>Email:<input type="text" name="email" /></li>
+		<li>Others:<textarea name="other" cols="45" rows="5"></textarea></li>
 		<input type="submit" name="button" value="submit" />
 		</form>
     </section>
--- register_finish.php
+++ register_finish.php
@@ -14,8 +14,10 @@ include("mysql_connect.inc.php");
 $id = $_POST['id'];
 $pw = $_POST['pw'];
 $pw2 = $_POST['pw2'];
+$gender=$_POST['Gender'];
 $telephone = $_POST['telephone'];
 $address = $_POST['address'];
+$email = $_POST['email'];
 $other = $_POST['other'];
 
 //判斷帳號密碼是否為空值
@@ -23,22 +25,22 @@ $other = $_POST['other'];
 if($id != null && $pw != null && $pw2 != null && $pw == $pw2)
 {
         //新增資料進資料庫語法
-        $sql = "insert into member_table (username, password, telephone, address, other) values ('$id', '$pw', '$telephone', '$address', '$other')";
+        $sql = "insert into member_table (username, password, Gender,telephone, address, Email, other) values ('$id', '$pw', '$gender','$telephone', '$address','$email', '$other')";
         if(mysql_query($sql))
         {
                 echo 'Successful!';
-                echo '<meta http-equiv=REFRESH CONTENT=2;url=index.php>';
+                //echo '<meta http-equiv=REFRESH CONTENT=2;url=index.php>';
         }
         else
         {
-                echo 'Sorry,try again!';
-                echo '<meta http-equiv=REFRESH CONTENT=2;url=index.php>';
+                echo 'Sorry,try again first!';
+                //echo '<meta http-equiv=REFRESH CONTENT=2;url=index.php>';
         }
 }
 else
 {
-        echo 'Sorry,try again!';
-        echo '<meta http-equiv=REFRESH CONTENT=2;url=index.php>';
+        echo 'Sorry,try again second!';
+       // echo '<meta http-equiv=REFRESH CONTENT=2;url=index.php>';
 }
 ?>
 </body>
